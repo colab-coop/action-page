9353453408ed78af3d4b54ee6e60d8d26e8e8b74
Ethan Winn <ethan@colab.coop>
2017-04-09 22:28:46 -0400

    Make config-dev generic

--- config-dev.js
+++ config-dev.js
@@ -1,23 +1,27 @@
 module.exports = {
   port: 1337,
   awsRegion: 'us-east-1',
-  awsBucket: 'awesomeactionpage.com',
-  awsCache: '/tmp/actionpage.cache',
+  awsBucket: 'actionpage.tech',
+  awsCache: '/tmp/actionpage.tech.cache',
   cloudFrontDistID: 'ABCD1234',
   title: 'Action-Page Title',
   mainlogo: 'logo.svg',
   actionNetwork: {
-    id: 'boycott-sunoco'
+    id: 'actionpage'
   },
   meta: {
     "og:title": 'Action-Page',
-    "og:url": 'https://awesomeactionpage.com/',
+    "og:url": 'https://actionpage.tech/',
     "og:description": 'This is a lengthier description of the action page',
-    "og:image": 'https://awesomeactionpage.com/actionpage.png',
+    "og:image": 'https://actionpage.tech/actionpage.png',
     "twitter:site": '@awesomeactionpage'
   },
+  sections: {
+    actionNetwork: false,
+    socialShare: false
+  },
   twitter: {
-    url: encodeURIComponent('https://awesomeactionpage.com/'),
+    url: encodeURIComponent('https://actionpage.tech/'),
     text: encodeURIComponent('A boilerplate for creating compelling, super-fast one-page action landings for Action Network campaigns using just HTML, CSS and AWS.'),
     related: 'actionpage',
     hashtags: 'actionpage, skeleton'
@@ -25,12 +29,12 @@ module.exports = {
   fb: {
     app_id: '573217829552127',
     pagetitle: 'Action Page',
-    ui_href: 'http://nosunoco.com/',
-    href: 'https://www.facebook.com/No-Sunoco-1243314592371578/'
+    ui_href: 'http://actionpage.tech/',
+    href: 'https://www.facebook.com/Action-Page-1111111111111111/'
   },
   google: {
     analytics: {
-      id: 'UA-87440490-1'
+      id: 'UA-11111111-1'
     }
   }
 }
